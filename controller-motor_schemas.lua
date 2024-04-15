42a8a3e27a779446d010cccc7eff660b96dab6f2
sceredi <ceredi.simone.iti@gmail.com>
2024-04-15 22:01:05 +0200

    feat: kinda working nicely now

--- controller-motor_schemas.lua
+++ controller-motor_schemas.lua
@@ -1,7 +1,6 @@
 -- Put your global variables here
 local robot_wrapper = require("src.wrapper.robot_wrapper")
 local logger = require("src.wrapper.logger")
-local perceptual_schemas = require("src.perceptual_schemas")
 local utils = require("src.utils")
 local vector = require("src.vector")
 
@@ -9,23 +8,22 @@ local MAX_VELOCITY = 15
 local D = 1
 
 local n_steps = 0
-local left_v = 0
-local right_v = 0
 
 --[[ This function is executed every time you press the 'execute'
      button ]]
 function init()
-	left_v = MAX_VELOCITY
-	right_v = 0
-	robot_wrapper.wheels.set_velocity(left_v, right_v)
 	n_steps = 0
 	logger.log("lets go")
+end
+
+local function uniform_field(length, angle)
+	return { length = length / D, angle = angle }
 end
 
 local function attraction_field(distance, angle)
 	local length = 0
 	if distance <= D then
-		length = (D - distance) / D
+		length = math.min(distance, D) / D
 	else
 		length = 0
 	end
@@ -35,57 +33,64 @@ end
 local function repulsive_field(distance, angle)
 	local length = 0
 	if distance <= D then
-		length = (D - distance) / D
+		length = math.min(distance, D) / D
 	else
 		length = 0
 	end
 	return { length = length, angle = -angle }
 end
 
-local function avoid_obstacles()
-	local closest_prox = perceptual_schemas.closest_proximity_sensor()
-	if closest_prox.value > 0.1 then
-		robot_wrapper.leds.set_all_colors("red")
-		return { distance = -closest_prox.value, angle = closest_prox.angle }
+local function tangential_field(distance, angle)
+	local length = 0
+	local angle_ret = 0
+	if angle > 0 then
+		angle_ret = angle - math.pi / 2
 	else
-		robot_wrapper.leds.set_all_colors("black")
-		return { distance = 0, angle = 0 }
+		angle_ret = angle + math.pi / 2
 	end
+	if distance <= D then
+		length = math.min(distance, D) / D
+	else
+		length = 0
+	end
+	return { length = length, angle = angle_ret }
+end
+
+local function avoid_obstacles()
+	local closest_prox = utils.closest_proximity_sensor()
+	return { distance = closest_prox.length, angle = closest_prox.angle }
 end
 
 local function go_towards_light()
-	local brightest_light = perceptual_schemas.brightest_light()
-	if brightest_light.value > 0.015 then
-		robot_wrapper.leds.set_all_colors("yellow")
-		return { distance = brightest_light.value + 0.5, angle = brightest_light.angle }
-	else
-		robot_wrapper.leds.set_all_colors("black")
-		return { distance = 0, angle = 0 }
-	end
+	local lights = utils.two_most_bright_lights()
+	local first = lights.first
+	local second = lights.second
+	local light = vector.vec2_polar_sum(first, second)
+	return { distance = light.length, angle = light.angle }
 end
 
 --[[ This function is executed at each time step
      It must contain the logic of your controller ]]
 function step()
 	n_steps = n_steps + 1
-	left_v = robot_wrapper.random.uniform(0, MAX_VELOCITY)
-	right_v = robot_wrapper.random.uniform(0, MAX_VELOCITY)
-	local left_vel = left_v
-	local right_vel = right_v
+	local left_vel = 0
+	local right_vel = 0
 	local obstacle_polar_val = avoid_obstacles()
 	local light_polar_val = go_towards_light()
-	local obstacle_polar = repulsive_field(obstacle_polar_val.distance, obstacle_polar_val.angle)
-	local light_polar = attraction_field(light_polar_val.distance, light_polar_val.angle)
-	local sum = vector.vec2_polar_sum(obstacle_polar, light_polar)
-	if sum.length > 0 then
-		left_vel, right_vel = utils.translational_to_differential(sum.length, sum.angle)
-	end
+	local go_straight = uniform_field(0.3, 0)
+	local obstacle_repulsion = repulsive_field(obstacle_polar_val.distance, obstacle_polar_val.angle)
+	local light_attraction = attraction_field(light_polar_val.distance * 2, light_polar_val.angle)
+	local obstacle_tangential = tangential_field(obstacle_polar_val.distance * 1.2, obstacle_polar_val.angle)
+	local sum = vector.vec2_polar_sum(go_straight, obstacle_repulsion)
+	logger.log("sum before light: " .. sum.length .. " angle: " .. sum.angle)
+	sum = vector.vec2_polar_sum(sum, light_attraction)
+	logger.log("sum after light: " .. sum.length .. " angle: " .. sum.angle)
+	sum = vector.vec2_polar_sum(sum, obstacle_tangential)
+	left_vel, right_vel = utils.translational_to_differential(sum.length, sum.angle)
 	local maxValue = math.max(left_vel, right_vel)
 	local scaleFactor = MAX_VELOCITY / maxValue
 	left_vel = left_vel * scaleFactor
 	right_vel = right_vel * scaleFactor
-	logger.log("left_vel: " .. left_vel .. "\nright_vel: " .. right_vel)
-	logger.log("\n\n")
 	robot_wrapper.wheels.set_velocity(left_vel, right_vel)
 end
 
@@ -95,10 +100,7 @@ end
      called. The state of sensors and actuators is reset
      automatically by ARGoS. ]]
 function reset()
-	left_v = MAX_VELOCITY
-	right_v = 0
 	-- robot.wheels.set_velocity(left_v, right_v)
-	robot_wrapper.wheels.set_velocity(left_v, right_v)
 	n_steps = 0
 end
 
--- src/utils.lua
+++ src/utils.lua
@@ -1,9 +1,69 @@
 local M = {}
+local robot_wrapper = require("src.wrapper.robot_wrapper")
+local vector = require("src.vector")
 
 function M.translational_to_differential(vel, ang)
 	local L = robot.wheels.axis_length
 	local left_v = vel - ang * L / 2
 	local right_v = vel + ang * L / 2
 	return left_v, right_v
+end
+
+function M.closest_proximity_sensor()
+	local closest = robot_wrapper.get_proximity_sensor_readings()[1]
+	for i = 2, 24 do
+		if i <= 6 or i >= 19 then
+			if robot_wrapper.get_proximity_sensor_readings()[i].value > closest.value then
+				closest = robot_wrapper.get_proximity_sensor_readings()[i]
+			end
+		end
+	end
+	return { length = closest.value, angle = closest.angle }
+end
+
+function M.brightest_light()
+	local brightest = robot_wrapper.get_light_sensor_readings()[1]
+	for i = 2, 24 do
+		if robot_wrapper.get_light_sensor_readings()[i].value > brightest.value then
+			brightest = robot_wrapper.get_light_sensor_readings()[i]
+		end
+	end
+	return { length = brightest.value, angle = brightest.angle }
+end
+
+function M.two_most_bright_lights()
+	local first = robot_wrapper.get_light_sensor_readings()[1]
+	local second = robot_wrapper.get_light_sensor_readings()[2]
+	if first.value < second.value then
+		first, second = second, first
+	end
+	for i = 3, 24 do
+		if robot_wrapper.get_light_sensor_readings()[i].value > first.value then
+			second = first
+			first = robot_wrapper.get_light_sensor_readings()[i]
+		elseif robot_wrapper.get_light_sensor_readings()[i].value > second.value then
+			second = robot_wrapper.get_light_sensor_readings()[i]
+		end
+	end
+	return {
+		first = { length = first.value, angle = first.angle },
+		second = { length = second.value, angle = second.angle },
+	}
+end
+
+function M.lights_sum()
+	local first = robot_wrapper.get_light_sensor_readings()[1]
+	local second = robot_wrapper.get_light_sensor_readings()[2]
+	local sum = vector.vec2_polar_sum(
+		{ length = first.value, angle = first.angle },
+		{ length = second.value, angle = second.angle }
+	)
+	for i = 3, 24 do
+		sum = vector.vec2_polar_sum(sum, {
+			length = robot_wrapper.get_light_sensor_readings()[i].value,
+			angle = robot_wrapper.get_light_sensor_readings()[i].angle,
+		})
+	end
+	return sum
 end
 return M
